8e6e66c732924d088c8be951a6873e1398a95469
Shirley Xia <xshirl1027@gmail.com>
2025-05-11 13:37:17 -0400

    undoing changes that breaks app

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 export function createTimeStampSToLyricsTable(lyrics: string[]) {
     const timeStampLyricsTable: [number, string][] = [];
-    for(const i in lyrics){
-        const { timestamp_ms, lyric } = parseLyricLine(lyrics[i]);
+    for(const line in lyrics){
+        const { timestamp_ms, lyric } = parseLyricLine(line);
             if (timestamp_ms !== null && timestamp_ms !== undefined) {
                 timeStampLyricsTable.push([timestamp_ms, lyric]);
             }
