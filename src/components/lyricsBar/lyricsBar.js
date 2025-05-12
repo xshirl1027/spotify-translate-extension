d175205bbe98df81b220ef224baa0c4871a2c0ca
Shirley Xia <xshirl1027@gmail.com>
2025-05-12 19:40:25 -0400

    Update lyricsBar.js

--- src/components/lyricsBar/lyricsBar.js
+++ src/components/lyricsBar/lyricsBar.js
@@ -1,24 +1,24 @@
 import React from "react";
 import styles from "./lyricsBar.module.css";
-//import { Translate } from "@google-cloud/translate";
-// const translateLine = async (text, targetLanguage) => {
-//   const translate = new Translate({ projectId });
-//   try {
-//     const [translation] = await translate.translate(text, targetLanguage);
-//     return translation;
-//   } catch (error) {
-//     console.error("Error translating text:", error);
-//     return text; // Fallback to original text in case of error
-//   }
-// };
+import { Translate } from "@google-cloud/translate";
+const translateLine = async (text, targetLanguage) => {
+  const translate = new Translate({ projectId });
+  try {
+    const [translation] = await translate.translate(text, targetLanguage);
+    return translation;
+  } catch (error) {
+    console.error("Error translating text:", error);
+    return text; // Fallback to original text in case of error
+  }
+};
 const LyricsBar = ({ currentLyrics }) => {
-  // const translatedLyrics = currentLyrics.map(async ([timestamp, line]) => {
-  //   const translatedLine = await translateLine(line, "fr");
-  //   if (translatedLine) {
-  //     console.log(translatedLine);
-  //   }
-  //   return [timestamp, line];
-  // });
+  const translatedLyrics = currentLyrics.map(async ([timestamp, line]) => {
+    const translatedLine = await translateLine(line, "fr");
+    if (translatedLine) {
+      console.log(translatedLine);
+    }
+    return [timestamp, line];
+  });
 
   if (currentLyrics === undefined || currentLyrics.length === 0) {
     return <></>;
