e7e112d4f5676d94514de41ebe911adb8750ed7c
Shirley Xia <xshirl1027@gmail.com>
2025-05-12 13:37:18 -0400

    Update nowPlayingBar.js

--- src/components/nowPlayingBar/nowPlayingBar.js
+++ src/components/nowPlayingBar/nowPlayingBar.js
@@ -5,19 +5,19 @@ import LyricsBar from "../lyricsBar/lyricsBar";
 const NowPlayingBar = ({ track, currentLyrics }) => {
   return (
     <div className={styles.nowPlayingBarContainer}>
-      {track ? (
-        <>
-          <LyricsBar currentLyrics={currentLyrics}></LyricsBar>
-          <div className={styles.nowPlayingBar}>
+      <LyricsBar currentLyrics={currentLyrics}></LyricsBar>
+      <div className={styles.nowPlayingBar}>
+        {track ? (
+          <>
             <p>
               <strong>{track.name}</strong> by {track.artists} -{" "}
               <em>{track.album}</em>
             </p>
-          </div>
-        </>
-      ) : (
-        <p>No Track is Currently Playing</p>
-      )}
+          </>
+        ) : (
+          <p>No Track is Currently Playing</p>
+        )}
+      </div>
     </div>
   );
 };
