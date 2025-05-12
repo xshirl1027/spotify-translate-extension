046ab43b9d651ebd33cee6be298bd385d529f4ef
Shirley Xia <xshirl1027@gmail.com>
2025-05-12 17:35:12 -0400

    Update lyricsBar.js

--- src/components/lyricsBar/lyricsBar.js
+++ src/components/lyricsBar/lyricsBar.js
@@ -1,17 +1,16 @@
 import React from "react";
 import styles from "./lyricsBar.module.css";
 import { Translate } from "@google-cloud/translate";
-const projectId = "spotify-translate-459619";
-const translateLine = async (text, targetLanguage) => {
-  const translate = new Translate({ projectId });
-  try {
-    const [translation] = await translate.translate(text, targetLanguage);
-    return translation;
-  } catch (error) {
-    console.error("Error translating text:", error);
-    return text; // Fallback to original text in case of error
-  }
-};
+// const translateLine = async (text, targetLanguage) => {
+//   const translate = new Translate({ projectId });
+//   try {
+//     const [translation] = await translate.translate(text, targetLanguage);
+//     return translation;
+//   } catch (error) {
+//     console.error("Error translating text:", error);
+//     return text; // Fallback to original text in case of error
+//   }
+// };
 const LyricsBar = ({ currentLyrics }) => {
   // const translatedLyrics = currentLyrics.map(async ([timestamp, line]) => {
   //   const translatedLine = await translateLine(line, "fr");
