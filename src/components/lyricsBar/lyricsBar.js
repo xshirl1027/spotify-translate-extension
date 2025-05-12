0eb6e954a0d81de8aa0b3b0f0f717118bf160b3a
Shirley Xia <xshirl1027@gmail.com>
2025-05-12 13:42:01 -0400

    Update lyricsBar.js

--- src/components/lyricsBar/lyricsBar.js
+++ src/components/lyricsBar/lyricsBar.js
@@ -2,18 +2,17 @@ import React from "react";
 import styles from "./lyricsBar.module.css";
 
 const LyricsBar = ({ currentLyrics }) => {
+  if (!currentlyrics) {
+    return;
+  }
   return (
-    <>
-      {currentLyrics && (
-        <div className={styles.lyricsBar}>
-          {currentLyrics.map((line) => (
-            <div key={line.id} className={styles.lyricsLine}>
-              <p>{line !== "" ? line : "♪  ... ♪"}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </>
+    <div className={styles.lyricsBar}>
+      currentLyrics.map((line) => (
+      <div key={line.id} className={styles.lyricsLine}>
+        <p>{line != "" ? line : "♪  ... ♪"}</p>
+      </div>
+      ))
+    </div>
   );
 };
 
