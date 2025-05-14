581c6fe98b3a4356a53a6afc9d0b7db162e8c75b
Shirley Xia <xshirl1027@gmail.com>
2025-05-13 20:21:13 -0400

    removing python start

--- start-services.sh
+++ start-services.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Check if Flask and Flask-CORS are installed
-if ! pip show flask > /dev/null 2>&1 || ! pip show flask-cors > /dev/null 2>&1; then
-  echo "Flask or Flask-CORS not found. Installing required packages..."
-  pip install flask flask-cors
-fi
-# Start the Flask backend
-echo "Starting Flask backend..."
-. venv/bin/activate # Activate the Python virtual environment
-python src/app.py > backend.log 2>&1 & # Run the Flask app in the background
-BACKEND_PID=$! # Save the process ID of the backend
-
 # Start the React frontend
 echo "Starting React frontend..."
 npm start > frontend.log 2>&1 & # Run npm start in the background
