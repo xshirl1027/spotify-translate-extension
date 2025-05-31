f591850f35746cd2bfc4e070234e0dd23435a4a4
Shirley Xia <xshirl1027@gmail.com>
2025-05-30 21:22:53 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -355,9 +355,9 @@ const pauseTrack = async () => {
           <div className={styles.listContainer}>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1rem' }}>
               {isPlaying ? (
-              <button onClick={pauseTrack} style={{ fontSize: '1.5rem', padding: '0.5rem 1.5rem' }}>⏸</button>
+              <button onClick={pauseTrack}>⏸</button>
               ) : (
-              <button onClick={() => playTrack(currentTrack)} style={{ fontSize: '1.5rem', padding: '0.5rem 1.5rem' }}>⏵︎</button>
+              <button onClick={() => playTrack(currentTrack)}>⏵︎</button>
               )}
             </div>
             <SearchResults searchResults={searchResults} onTrackClick={onTrackClick} trackClickDisabled={trackCickDisabled} onTrackPlay={playTrack}/>
