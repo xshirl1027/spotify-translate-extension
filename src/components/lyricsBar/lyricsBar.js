9678c5a01f3c9411126ae69ef8a4472d2bb01651
Shirley Xia <xshirl1027@gmail.com>
2025-06-01 21:25:15 -0400

    Update lyricsBar.js

--- src/components/lyricsBar/lyricsBar.js
+++ src/components/lyricsBar/lyricsBar.js
@@ -52,11 +52,15 @@ const LyricsBar = ({ currentLyrics }) => {
           {/* Add more languages as needed */}
         </select>
       </div>
-      {translatedLyrics.slice(0, -1).map(([timestamp, line], idx) => (
-        <div key={timestamp || idx} className={styles.lyricsLine}>
-          <p>{line !== "" ? decodeHtmlEntities(line) : "♪ ... ♪"}</p>
-        </div>
-      ))}
+      {!translatedLyrics || translatedLyrics.length < 2 ? (
+        <p>{line !== "" ? decodeHtmlEntities(line) : "♪ ... ♪"}</p>
+      ) : (
+        translatedLyrics.slice(0, -1).map(([timestamp, line], idx) => (
+          <div key={timestamp || idx} className={styles.lyricsLine}>
+            <p>{line !== "" ? decodeHtmlEntities(line) : "♪ ... ♪"}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
